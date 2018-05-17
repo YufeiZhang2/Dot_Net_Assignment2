c87df556f0f7cefed84c4e1bbf89cac6e9a17e98
AmiiiiiiZ <AmiZhou@users.github.com>
2018-05-18 03:20:22 +1000

    Fixed question asking function finished.
    Tried some connection strings configuration, but still not sure if it is fine.
    Fixed question asking function finished.
    Related tests finished.

---
+++ Assignment2/Assignment2.Bussiness/Model/Model.cs
@@ -0,0 +1,87 @@
+namespace Assignment2.Bussiness
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using SqlProviderServices = System.Data.Entity.SqlServer.SqlProviderServices;
+
+    public partial class Model : DbContext
+    {
+        public Model()
+            : base("name=Database")
+        {
+        }
+
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<AspNetRoles> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaims> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogins> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUsers> AspNetUsers { get; set; }
+        public virtual DbSet<DataDrivenRules> DataDrivenRules { get; set; }
+        public virtual DbSet<FixedRules> FixedRules { get; set; }
+        public virtual DbSet<WeatherInfo> WeatherInfo { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AspNetRoles>()
+                .HasMany(e => e.AspNetUsers)
+                .WithMany(e => e.AspNetRoles)
+                .Map(m => m.ToTable("AspNetUserRoles").MapLeftKey("RoleId").MapRightKey("UserId"));
+
+            modelBuilder.Entity<AspNetUsers>()
+                .HasMany(e => e.AspNetUserClaims)
+                .WithRequired(e => e.AspNetUsers)
+                .HasForeignKey(e => e.UserId);
+
+            modelBuilder.Entity<AspNetUsers>()
+                .HasMany(e => e.AspNetUserLogins)
+                .WithRequired(e => e.AspNetUsers)
+                .HasForeignKey(e => e.UserId);
+
+            modelBuilder.Entity<DataDrivenRules>()
+                .Property(e => e.Question)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<DataDrivenRules>()
+                .Property(e => e.QuestionColumn)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<DataDrivenRules>()
+                .Property(e => e.Answer)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<DataDrivenRules>()
+                .Property(e => e.AnswerColumn)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<DataDrivenRules>()
+                .Property(e => e.CurrentStatus)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<DataDrivenRules>()
+                .Property(e => e.LastEditorID)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<FixedRules>()
+                .Property(e => e.Question)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<FixedRules>()
+                .Property(e => e.Answer)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<FixedRules>()
+                .Property(e => e.CurrentStatus)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<FixedRules>()
+                .Property(e => e.LastEditorID)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<WeatherInfo>()
+                .Property(e => e.LastMaintainerID)
+                .IsUnicode(false);
+        }
+    }
+}
---
+++ Assignment2/Assignment2.UnitTests/QuestionTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+using Assignment2.Bussiness;
+
+namespace Assignment2.UnitTests
+{
+    [TestFixture]
+    class EditorQuestionTests
+    {
+        RulesEditor rulesEditor;
+        QuestionManager questionManager;
+
+        /// <summary>
+        /// Instantiate editor and manager classes.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            rulesEditor = new RulesEditor();
+            questionManager = new QuestionManager();
+        }
+
+        /// <summary>
+        /// Use invalid questions to receive null answers.
+        /// </summary>
+        /// <param name="question"> Various questions. </param>
+        [Test]
+        [TestCase("I asked an invalid question here...")]
+        [TestCase("Another invalid question.")]
+        [TestCase("What is the temperature on this *?!")]
+        [TestCase("What is the day when it is 99 Celsius degree?")]
+        [TestCase("What is the day when it is NaN Celsius degree?")]
+        public void GetAnswerToAQuestion_InvalidQuestion_ReturnNull(string question)
+        {
+            string result = questionManager.TryGetAnswer(question);
+
+            Assert.That(result == null);
+        }
+
+        /// <summary>
+        /// Use invalid questions to receive null answers.
+        /// </summary>
+        /// <param name="question"> Various questions. </param>
+        [Test]
+        [TestCase("hello")]
+        [TestCase("hel???lo")]
+        [TestCase("hello#%@")]
+        public void GetAnswerToAQuestion_ValidQuestion_ReturnNotNull(string question)
+        {
+            string result = questionManager.TryGetAnswer(question);
+
+            Assert.That(result == "Hi! How are you?");
+        }
+    }
+}
