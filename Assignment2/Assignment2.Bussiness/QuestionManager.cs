b0447cfc064c256f88a6e4b683f5c1e58e45ea73
AmiiiiiiZ <AmiZhou@users.github.com>
2018-05-18 21:12:51 +1000

    QuestionManager one function created + tested.
    - Configured the UnitTest layer.
    - Made a new database for testing (DatabaseTest.mdf, copied from Database.mdf), but now the database in use is still the old database. To use the test version we may need to change the connection string.
    - Made a test class for question functions. The implemented function passed the test successfully.

---
+++ Assignment2/Assignment2.Bussiness/QuestionManager.cs
@@ -0,0 +1,62 @@
+﻿using Assignment2.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Assignment2.Bussiness
+{
+    class AnswerToQuestion
+    {
+        public string answer;
+        public string question;
+    }
+
+    public class QuestionManager
+    {
+
+        public string TryGetAnswer(string question)
+        {
+            // Check simple rules first
+            // Clean the spaces of the question.
+            string lowerUserQuestion = question.ToLower().Trim();
+            string cleanUserQuestion = Regex.Replace(lowerUserQuestion, @"[^a-zA-Z0-9]", string.Empty, RegexOptions.Compiled);
+
+
+            using (Context context = new Context())
+            {
+                //var simpleAnswer = from rule in context.FixedRules
+                //                  where rule.Question == cleanUserQuestion
+                //                  select rule.Answer.FirstOrDefault();
+                //rule.Question.ToLower().Trim().Replace("!","")
+                var answers = context.FixedRules
+                    .Where(rule => rule.CurrentStatus == "Approved").ToList()
+                    .Select(
+                    rule => new AnswerToQuestion
+                    {
+                        question = Regex.Replace(rule.Question.ToLower().Trim(), @"[^a-zA-Z0-9]", string.Empty, RegexOptions.Compiled),
+                        answer = rule.Answer
+                    }
+                    );
+
+                var simpleAnswer = answers.Where(rule => rule.question == cleanUserQuestion).Select(rule => rule.answer);
+
+                if (simpleAnswer.Count() > 0)
+                {
+                    return simpleAnswer.First();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+        }
+
+
+    }
+
+}
---
+++ Assignment2/Assignment2.UnitTests/QuestionTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+using Assignment2.Bussiness;
+
+namespace Assignment2.UnitTests
+{
+    [TestFixture]
+    class EditorQuestionTests
+    {
+        QuestionManager questionManager;
+
+        /// <summary>
+        /// Instantiate editor and manager classes.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            questionManager = new QuestionManager();
+        }
+
+        /// <summary>
+        /// Use invalid questions to receive null answers.
+        /// </summary>
+        /// <param name="question"> Various questions. </param>
+        [Test]
+        [TestCase("Damn")]
+        [TestCase("I asked an invalid question here...")]
+        [TestCase("Another invalid question.")]
+        [TestCase("What is the temperature on this *?!")]
+        [TestCase("What is the day when it is 99 Celsius degree?")]
+        [TestCase("What is the day when it is NaN Celsius degree?")]
+        public void GetAnswerToAQuestion_InvalidQuestion_ReturnNull(string question)
+        {
+            string result = questionManager.TryGetAnswer(question);
+
+            Assert.That(result == null);
+        }
+
+        /// <summary>
+        /// Use invalid questions to receive null answers.
+        /// </summary>
+        /// <param name="question"> Various questions. </param>
+        [Test]
+        [TestCase("hello")]
+        [TestCase("hel???lo")]
+        [TestCase("hello#%@")]
+        public void GetAnswerToAQuestion_ValidQuestion_ReturnNotNull(string question)
+        {
+            string result = questionManager.TryGetAnswer(question);
+
+            Assert.That(result == "Hi! How are you?");
+        }
+    }
+}
