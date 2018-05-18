53621fb51263bd7d5e4f12900f9bc20fae30766c
Crys Bui <crysbui.depon@gmail.com>
2018-05-18 15:06:58 +1000

    Create a code-first Entity Framework context in Web MVC layer
    - Create WeatherController
    - Create Views for different pages using Shared/_Layout (similar to Site.Master in Web form)

---
+++ Assignment2/Assignment2.WebApplication/Controllers/WeatherController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2.WebApplication.Controllers
+{
+    public class WeatherController : Controller
+    {
+        // GET: Weather
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DataMaintainer()
+        {
+            return View();
+        }
+
+        public ActionResult RuleEditor()
+        {
+            return View();
+        }
+
+        public ActionResult RuleApprover()
+        {
+            return View();
+        }
+    }
+}
