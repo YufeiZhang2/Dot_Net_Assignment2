264e682d990a915be9337f785202dee87515dcf5
Crys Bui <crysbui.depon@gmail.com>
2018-05-25 02:25:56 +1000

    test 1

---
+++ Assignment2/Assignment2.Bussiness/DataDrivenRuleEditor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment2.Database;
+
+namespace Assignment2.Bussiness
+{
+    public class DataDrivenRuleEditor
+    {
+        public void AddDataDriveRule(DataDrivenRules rule)
+        {
+            using (var context = new Context())
+            {
+                context.DataDrivenRules.Add(rule);
+                context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<DataDrivenRules> AllDataDrivenRules
+        {
+            get
+            {
+                using (var context = new Context())
+                {
+                    // Materialize to a list because the context is closed
+                    return context.DataDrivenRules.ToList();
+                }
+            }
+        }
+    }
+}
--- Assignment2/Assignment2.WebApplication/Controllers/EditorController.cs
+++ Assignment2/Assignment2.WebApplication/Controllers/EditorController.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Assignment2.Database;
+using Assignment2.Bussiness;
+using Assignment2.WebApplication.Models;
+
 
 namespace Assignment2.WebApplication.Controllers
 {
     public class EditorController : Controller
     {
+        //private DataDrivenRuleEditor rules = new DataDrivenRuleEditor();
+
+        private ApplicationDbContext _context;
+
+        public EditorController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Editor
         public ActionResult Index()
+        {
+            var rules = _context.DataDrivenRules;
+            return View(rules);
+        }
+
+        public ActionResult AddDataDriveRule()
         {
             return View();
         }
