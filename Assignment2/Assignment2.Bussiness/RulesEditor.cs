5ea6e0b1382e502d2cbb3c1ecd702383940793c8
AmiiiiiiZ <AmiZhou@users.github.com>
2018-05-25 16:41:53 +1000

    Add back Editor related views and functions
    Additionally, I changed some overall css styles of  the site.

---
+++ Assignment2/Assignment2.Bussiness/RulesEditor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment2.Database;
+
+namespace Assignment2.Bussiness
+{
+
+    public class RulesEditor
+    {
+        public List<FixedRules> GetFixedRulesByStatus(string status)
+        {
+            using (Context context = new Context())
+            {
+                var rules = context.FixedRules
+                    .Where(rule => rule.CurrentStatus == status).ToList();
+                return rules;
+            }
+        }
+
+        public List<DataDrivenRules> GetDataDrivenRulesByStatus(string status)
+        {
+            using (Context context = new Context())
+            {
+                var rules = context.DataDrivenRules
+                    .Where(rule => rule.CurrentStatus == status).ToList();
+                return rules;
+            }
+        }
+
+        public List<FixedRules> GetYourFixedRulesByStatus(string status, string username)
+        {
+            using (Context context = new Context())
+            {
+                var rules = context.FixedRules
+                    .Where(rule => rule.CurrentStatus == status && rule.LastEditorID == username).ToList();
+                return rules;
+            }
+        }
+
+        public List<DataDrivenRules> GetYourDataDrivenRulesByStatus(string status, string username)
+        {
+            using (Context context = new Context())
+            {
+                var rules = context.DataDrivenRules
+                    .Where(rule => rule.CurrentStatus == status && rule.LastEditorID == username).ToList();
+                return rules;
+            }
+        }
+
+
+
+        public void AddFixedRule()
+        {
+
+            using (Context context = new Context())
+            {
+
+                context.SaveChanges();
+            }
+        }
+
+
+        public void UpdateFixedRule()
+        {
+
+            using (Context context = new Context())
+            {
+
+                context.SaveChanges();
+            }
+
+        }
+
+        public void DeleteFixedRule()
+        {
+
+            using (Context context = new Context())
+            {
+
+                context.SaveChanges();
+            }
+
+        }
+
+
+    }
+}
--- Assignment2/Assignment2.WebApplication/Controllers/EditorController.cs
+++ Assignment2/Assignment2.WebApplication/Controllers/EditorController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Assignment2.Bussiness;
+using Assignment2.Database;
+
 
 namespace Assignment2.WebApplication.Controllers
 {
+    public class AllLists
+    {
+        public List<FixedRules> ApprovedFixedRules;
+        public List<DataDrivenRules> ApprovedDataDrivenRules;
+        public List<FixedRules> RejectedFixedRules;
+        public List<DataDrivenRules> RejectedDataDrivenRules;
+        public List<FixedRules> UncheckedFixedRules;
+        public List<DataDrivenRules> UncheckedDataDrivenRules;
+    }
+
     public class EditorController : Controller
     {
+        private RulesEditor rulesEditor = new RulesEditor();
+
         // GET: Editor
         public ActionResult Index()
         {
-            return View();
+            AllLists allLists = new AllLists();
+
+            allLists.RejectedDataDrivenRules = rulesEditor.GetDataDrivenRulesByStatus("Rejected");
+            allLists.UncheckedDataDrivenRules = rulesEditor.GetDataDrivenRulesByStatus("Unchecked");
+
+            allLists.RejectedFixedRules = rulesEditor.GetFixedRulesByStatus("Rejected");
+            allLists.UncheckedFixedRules = rulesEditor.GetFixedRulesByStatus("Unchecked");
+
+            return View(allLists);
         }
 
         public ActionResult AddDataDrivenRule()
@@ -22,6 +45,20 @@ namespace Assignment2.WebApplication.Controllers
         public ActionResult AddFixedRule()
         {
             return View();
+        }
+
+
+
+
+
+        public ActionResult EditorReport()
+        {
+            AllLists allLists = new AllLists();
+
+            allLists.ApprovedDataDrivenRules = rulesEditor.GetDataDrivenRulesByStatus("Approved");
+            allLists.ApprovedFixedRules = rulesEditor.GetFixedRulesByStatus("Approved");
+
+            return View(allLists);
         }
     }
 }
