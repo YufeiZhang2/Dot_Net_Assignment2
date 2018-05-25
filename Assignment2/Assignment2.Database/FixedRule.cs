fedf3e1f5a2f94729baa81c78901a3660ee25ba5
Yufei Zhang <yufei.z222@gmail.com>
2018-05-25 14:17:30 +1000

    3 views of approver completed

---
+++ Assignment2/Assignment2.Database/FixedRule.cs
@@ -0,0 +1,27 @@
+namespace Assignment2.Database
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class FixedRule
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Question { get; set; }
+
+        [Required]
+        public string Answer { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string CurrentStatus { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastEditorID { get; set; }
+    }
+}
