e2749bb8ccceab9adbd8520e6ed679b941304532
Jen Labayno <jenlabayno@gmail.com>
2018-05-29 15:31:01 +1000

    Updated Maintainer
    Remove the Maintainer that was created automatically
    Added Business Logic for Maintainer

--- Assignment2/Assignment2.WebApplication/Controllers/MaintainerController.cs
+++ Assignment2/Assignment2.WebApplication/Controllers/MaintainerController.cs
@@ -2,126 +2,65 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Core.Mapping;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Assignment2.Bussiness;
+using Assignment2.Database;
 using Assignment2.WebApplication.Models;
 
 namespace Assignment2.WebApplication.Controllers
 {
+    public class MaintainerData
+    {
+        public List<WeatherInfo> AllWeatherInfos;
+
+
+    }
+
     public class MaintainerController : Controller
     {
-        private DatabaseContext db = new DatabaseContext();
+        private Maintainer maintainer = new Maintainer();
 
         // GET: Maitainer
+        /*    public ActionResult Index()
+            {
+                List<WeatherInfo> AllWeatherInfos = Maintainer.GetWeatherInfos().ToList();
+
+                    return View();
+                }
+    */
+
         public ActionResult Index()
         {
-            return View(db.Maintainer.ToList());
+            return View(
+                new MaintainerViewModel
+                {
+                    WeatherInfos = Maintainer.GetWeatherInfos().ToList()
+                }
+            );
         }
-
-        // GET: Maitainer/Details/5
-        public ActionResult Details(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Maintainer maitainer = db.Maintainer.Find(id);
-            if (maitainer == null)
-            {
-                return HttpNotFound();
-            }
-            return View(maitainer);
-        }
-
         // GET: Maitainer/Save
-        public ActionResult Save()
-        {
-            return View();
-        }
-
-        // POST: Maitainer/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Save([Bind(Include = "Day,Weather,Outfit,Temperature,LastMaintainerId")] Maintainer maitainer)
+        public ActionResult Save(MaintainerViewModel model)
         {
-            if (ModelState.IsValid)
-            {
-                db.Maintainer.Add(maitainer);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
 
-            return View(maitainer);
-        }
-
-        // GET: Maitainer/Edit/5
-        public ActionResult Edit(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Maintainer maitainer = db.Maintainer.Find(id);
-            if (maitainer == null)
-            {
-                return HttpNotFound();
-            }
-            return View(maitainer);
-        }
-
-        // POST: Maitainer/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Day,Weather,Outfit,Temperature,LastMaintainerId")] Maintainer maitainer)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(maitainer).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(maitainer);
-        }
-
-        // GET: Maitainer/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Maintainer maitainer = db.Maintainer.Find(id);
-            if (maitainer == null)
-            {
-                return HttpNotFound();
-            }
-            return View(maitainer);
-        }
-
-        // POST: Maitainer/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            Maintainer maitainer = db.Maintainer.Find(id);
-            db.Maintainer.Remove(maitainer);
-            db.SaveChanges();
+            Maintainer.AddWatherInfo
+            (new WeatherInfo
+                {
+                    Day = model.Day,
+                    Weather = model.Weather,
+                    Outfit = model.Outfit,
+                    Temperature = model.Temperature
+                }
+            );
             return RedirectToAction("Index");
         }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
     }
+
+
+
 }
---
+++ Assignment2/Assignment2.WebApplication/Models/MaintainerViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Assignment2.WebApplication.Models
+{
+    public class MaintainerViewModel
+    {
+        [Key]
+        [Required]
+        [StringLength(10)]
+        public string Day { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Weather { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Outfit { get; set; }
+
+        [Required]
+        public int Temperature { get; set; }
+
+        public string LastMaintainerId { get; set; }
+    }
+
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<MaintainerViewModel> Maintainer { get; set; }
+    }
+}
