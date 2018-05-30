d085762b87b98cc99265bbfbeccfb67655fd94c4
Jen Labayno <jenlabayno@gmail.com>
2018-05-30 23:00:43 +1000

    All Functions for Maintainer is working
    -Added edit function and delete function
    -Added some validation to handle error

--- Assignment2/Assignment2.Bussiness/Maintainer.cs
+++ Assignment2/Assignment2.Bussiness/Maintainer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,34 +18,23 @@ namespace Assignment2.Bussiness
         /// Get all weather data from WeatherInfo table
         /// </summary>
         /// <returns>weather data</returns>
-        public static List<WeatherInfo> GetWeatherInfos()
+        public static List<WeatherInfo> GetWeatherInfos(string username)
         {
             using (Context context = new Context())
-            {      
+            {
+                
                 return context.WeatherInfo.ToList();
             }
 
-        }
-
-        public IEnumerable<WeatherInfo> GetWeatherInfo
-        {
-            get
-            {
-                using (Context context = new Context())
-                {
-                    // Materialize to a list because the context is closed
-                    return context.WeatherInfo.ToList();
-                }
-            }
         }
 
         public static void AddWeatherInfo(WeatherInfo weatherInfo)
         {
             using (Context context = new Context())
             {
-                
-                    context.WeatherInfo.Add(weatherInfo);
-                    context.SaveChanges();
+
+                context.WeatherInfo.Add(weatherInfo);
+                context.SaveChanges();
 
             }
         }
@@ -53,16 +43,17 @@ namespace Assignment2.Bussiness
         /// Remove a data in Weatherinfo
         /// </summary>
         /// <param name="weatherInfo">The contact to remove</param>
-        public void DeleteWeatherInfo(WeatherInfo weatherInfo)
+        public void Delete(WeatherInfo weatherInfo)
         {
             using (var context = new Context())
             {
-                var saved = context.WeatherInfo.Find(weatherInfo.Day);
+                var saved = context.WeatherInfo.Find(weatherInfo.Id);
                 context.WeatherInfo.Remove(saved);
                 context.SaveChanges();
 
             }
         }
+
 
         /// <summary>
         /// Updates weatherinfo
@@ -72,10 +63,28 @@ namespace Assignment2.Bussiness
         {
             using (var context = new Context())
             {
-                var stored = context.WeatherInfo.Find(weatherInfo.Day);
-                context.Entry(stored).CurrentValues.SetValues(weatherInfo);
-                context.SaveChanges();
+                var updated = context.WeatherInfo.Find(weatherInfo.Id);
+                if (updated != null)
+                {
+
+                    context.Entry(updated).CurrentValues.SetValues(weatherInfo);
+                    context.SaveChanges();
+                }
+               
             }
+        }
+
+        public WeatherInfo SearchById(int id)
+        {
+            using (var context = new Context())
+            {
+                return (from w in context.WeatherInfo
+                    where w.Id == id
+                    select w).FirstOrDefault();
+
+            }
+
         }
     }
 }
+    
